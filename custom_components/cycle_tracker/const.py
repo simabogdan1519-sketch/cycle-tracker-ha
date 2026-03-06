69da6167ab2f4c1edcd3060f10f2753df5b2bc5d
simabogdan1519-sketch <sima.bogdan1519@gmail.com>
2026-03-06 23:10:21 +0200

    v1.0.3

---
+++ custom_components/cycle_tracker/const.py
@@ -0,0 +1,24 @@
+DOMAIN = "cycle_tracker"
+
+CONF_NAME = "name"
+CONF_CYCLE_START = "cycle_start_date"
+CONF_CYCLE_LENGTH = "cycle_length"
+CONF_PERIOD_LENGTH = "period_length"
+CONF_NOTIFY_DEVICE = "notify_device"
+CONF_NOTIFY_PERIOD = "notify_period"
+CONF_NOTIFY_OVULATION = "notify_ovulation"
+CONF_NOTIFY_DAILY = "notify_daily"
+
+DEFAULT_CYCLE_LENGTH = 28
+DEFAULT_PERIOD_LENGTH = 5
+
+PHASE_MENSTRUATIE = "menstruatie"
+PHASE_FOLICULARA  = "foliculara"
+PHASE_OVULATIE    = "ovulatie"
+PHASE_LUTEALA     = "luteala"
+
+FERTILITY_SCAZUT      = "scazut"
+FERTILITY_MODERAT     = "moderat"
+FERTILITY_INALT       = "inalt"
+FERTILITY_FOARTE_INALT= "foarte_inalt"
+FERTILITY_MAXIM       = "maxim"
